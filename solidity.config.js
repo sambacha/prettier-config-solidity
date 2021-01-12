cc5bde36c3faa291a60d16ee79540b137188031e
sam bacha <sam@freighttrust.com>
2021-01-12 02:18:32 -0800

    release: 1..0.0

--- solidity.config.js
+++ solidity.config.js
@@ -1,15 +1,22 @@
-{
-  overrides: [
-    {
-      files: '*.sol',
-      options: {
-        printWidth: 145,
-        tabWidth: 4,
-        useTabs: false,
-        singleQuote: false,
-        bracketSpacing: false,
-        explicitTypes: 'always'
-      }
-    }
-  ]
-}
+// Prettier Configuration for Solidity
+// Settings based from Solidity Documentation 
+module.exports = {
+	"arrowParens": "avoid",
+	"bracketSpacing": true,
+	"endOfLine": "auto",
+	"printWidth": 120,
+	"singleQuote": false,
+	"tabWidth": 2,
+	"trailingComma": "all",
+	overrides: [{
+		files: '*.sol',
+		options: {
+			printWidth: 145,
+			tabWidth: 4,
+			useTabs: false,
+			singleQuote: false,
+			bracketSpacing: false,
+			explicitTypes: 'always'
+		}
+	}]
+};
