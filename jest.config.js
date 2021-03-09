a2b40e51e99a018273dee01816781c0aa5e7de3a
sam <sam@freighttrust.com>
2021-03-09 11:24:43 -0800

    fix: npm
    Signed-off-by: sam <sam@freighttrust.com>

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,29 @@
+
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    "scripts/**/*.js",
-    "!scripts/generateIndexes.js",
-    "src/**/*.js",
-    "!<rootDir>/node_modules/",
-    "!src/prettier-comments/**/*.js",
+    'scripts/**/*.js',
+    '!scripts/generateIndexes.js',
+    'src/**/*.js',
+    '!<rootDir>/node_modules/',
+    '!src/prettier-comments/**/*.js'
   ],
-  coverageDirectory: "./coverage/",
-  setupFiles: ["<rootDir>/tests_config/run_spec.js"],
-  snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
-  testEnvironment: "node",
-  testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$|scripts/.*\\.test\\.js$",
+  coverageDirectory: './coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  setupFiles: ['<rootDir>/tests_config/run_spec.js'],
+  snapshotSerializers: ['<rootDir>/tests_config/raw-serializer.js'],
+  testEnvironment: 'node',
+  testRegex: 'jsfmt\\.spec\\.js$|__tests__/.*\\.js$|scripts/.*\\.test\\.js$',
   transform: {},
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
 };
