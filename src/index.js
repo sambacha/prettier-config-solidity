cb7ee07d9e4d9c63dc7563a7735a4a04b936e4a4
sam bacha <sam@freighttrust.com>
2021-03-31 18:21:43 -0700

    fix(solidity): enumerate all possible settings
    create settings for every possible standard prettier option. 
    all settings are set as to minimize the amount of AST diff-churn in diffing between git commits.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 /**
  * @file Prettier configuration for Solidity
- * @version 1.0.8
+ * @version 1.1.0
  * @summary base config adapted from AirBNB to minizmie diff churn
  * @overrides solidity settings from Solidity Documentation
+ * @note `printWidth` is not a line character capture
+ * @dev `explicitTypes`
  */
+
+'use strict';
 
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
   endOfLine: 'lf',
-  printWidth: 100,
+  printWidth: 80,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
+  quoteProps: 'as-needed',
+  proseWrap: 'always',
+  semi: true,
   overrides: [
     {
       files: '*.sol',
       options: {
-        printWidth: 120,
+        printWidth: 80,
         tabWidth: 4,
         useTabs: false,
         singleQuote: false,
-        bracketSpacing: false,
+        bracketSpacing: true,
         explicitTypes: 'always',
       },
     },
