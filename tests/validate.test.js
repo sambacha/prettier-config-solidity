d68bf3a598827cbfa89697942025e766df41ecb2
sam bacha <sam@manifoldfinance.com>
2021-09-13 08:05:17 -0700

    tests(validate): ensure config validates
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

---
+++ tests/validate.test.js
@@ -0,0 +1,49 @@
+const Ajv = require("ajv");
+// options can be passed, e.g. {allErrors: true}
+const path = require('path');
+
+//import schema from 'schema.json';
+const schema = require('./schema/prettier.schema.json');
+
+
+const configSolidity = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+  printWidth: 80,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  quoteProps: 'as-needed',
+  proseWrap: 'always',
+  semi: true,
+  overrides: [
+    {
+      files: '*.sol',
+      options: {
+        printWidth: 80,
+        tabWidth: 4,
+        useTabs: false,
+        singleQuote: false,
+        bracketSpacing: true,
+        explicitTypes: 'always',
+      },
+    },
+  ],
+};
+
+
+function isValid(schema, configSolidity) {
+  const ajv = new Ajv();
+  const valid = ajv.validate(schema, configSolidity);
+
+  if (!valid) {
+    console.log(ajv.errors);
+    return false;
+  };
+
+  return true;
+  console.log('Successfully Validated Configuration File');
+};
