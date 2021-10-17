67c63bc68c882400a2860f5998c581001ed491ee
sam bacha <sam@manifoldfinance.com>
2021-10-16 18:30:32 -0700

    refactor(package): finalize refactor

--- src/solidity.js
+++ src/solidity.js
@@ -1,6 +1,6 @@
 /**
  * @file Prettier configuration for Solidity
- * @version 1.3.0
+ * @version 1.3.1
  * @summary base config adapted from AirBNB to reduce diff churn
  * @overrides solidity settings from Solidity Documentation
  * @note `printWidth` is not a line character capture
@@ -13,21 +13,18 @@
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
   endOfLine: 'lf',
   printWidth: 80,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
   quoteProps: 'as-needed',
-  proseWrap: 'always',
   semi: true,
   overrides: [
     {
       files: '*.sol',
       options: {
-        printWidth: 80,
+        printWidth: 100,
         tabWidth: 4,
         useTabs: false,
         singleQuote: false,
