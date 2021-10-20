b6612f7f371ac1aa988862c3222988ac41d393c2
sam bacha <sam@manifoldfinance.com>
2021-10-20 07:13:29 -0700

    refactor(tests): e2e

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,30 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const FULL_TEST = Boolean(process.env.FULL_TEST);
-const TEST_STANDALONE = Boolean(process.env.TEST_STANDALONE);
+// jest.config.js
+const {defaults} = require('jest-config');
 
+const CONSOLE_FAIL_TYPES = ['error', 'warn']
+
+// Throw errors when a `console.error` or `console.warn` happens
+// by overriding the functions
+CONSOLE_FAIL_TYPES.forEach((type) => {
+  console[type] = (message) => {
+    throw new VError.errorFromList(
+      `Failing due to console.${type} while running test!\n\n${message}`,
+    )
+  }
+})
 module.exports = {
   collectCoverage: FULL_TEST,
   collectCoverageFrom: [
-    'scripts/**/*.js',
+    "__tests__/**/*.js",
+    '!scripts/**/*.js',
     '!scripts/generateIndexes.js',
     'src/**/*.js',
     '!<rootDir>/node_modules/',
-    '!src/prettier-comments/**/*.js',
+    '!src/prettier-comments/**/*.js'
   ],
+  "coverageReporters": ["clover", "json", "lcov"],
   coverageDirectory: './coverage/',
   coveragePathIgnorePatterns: ['/node_modules/', '/scripts/'],
   coverageThreshold: {
@@ -17,18 +32,20 @@ module.exports = {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100,
-    },
-  },
-  moduleNameMapper: {
-    '^prettier$': TEST_STANDALONE
-      ? '<rootDir>/node_modules/prettier/standalone'
-      : '<rootDir>/node_modules/prettier',
+      statements: 100
+    }
   },
   setupFiles: ['<rootDir>/tests/config/setup.js'],
-  snapshotSerializers: ['jest-snapshot-serializer-raw', 'jest-snapshot-serializer-ansi'],
+  snapshotSerializers: [
+    'jest-snapshot-serializer-raw',
+    'jest-snapshot-serializer-ansi'
+  ],
+  testTimeout: 12500,
   testEnvironment: 'node',
   testRegex: 'jsfmt\\.spec\\.js$|tests/unit/.*\\.js$',
   transform: {},
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
 };
