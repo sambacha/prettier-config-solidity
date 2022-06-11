defe8c1cba60ff1673f23094d1ca615e4c9ff9b1
sam bacha <sam@manifoldfinance.com>
2022-06-11 05:06:48 -0700

    fix(prettier): refactor config for import sorts support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 /**
- * @file Prettier configuration for Solidity
- * @version 1.3.1
- * @summary base config adapted from AirBNB to reduce diff churn
- * @overrides solidity settings from Solidity Documentation
+ * @package prettier-config-solidity
+ * @description Prettier configuration for Solidity
+ * @version 1.8.0
+ * 
+ * @summary base config adapted from AirBNB to reduce AST Diff Churn.
+ *    Additionally, this overrides the suggested settings from 
+ *     the reference Solidity Documentation.
+ *
+ * @note Solidity does not support dangling commas, which would greatly
+ *    improve the stated objectives of this configuration file
  * @note `printWidth` is not a line character capture
- * @dev `explicitTypes`
- * @solidity versions ^0.8.0 bytes1
+ *
+ * @note `explicitTypes` are enforced for ABI conformance
+ *    meaning `unit` is turned to `uint256`
+ *
+ * @note In Solidity versions ^0.8.0, bytes1 is removed
+ *
  */
 
 'use strict';
+
+/** 
+* @exports prettier-config-solidity 
+*/
 
 /**
  * @type {import('prettier').Options}
@@ -25,7 +39,7 @@ module.exports = {
   quoteProps: 'as-needed',
   semi: true,
 plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
-importOrder: ["^@forge-std/(.*)$", "^ds-test/(.*)$", "^@openzeppelin/(.*)$"","^~(.*)$", "^[./]"],
+importOrder: ["^@forge-std/(.*)$", "^ds-test/(.*)$", "^@openzeppelin/(.*)$","^~(.*)$", "^[./]"],
 importOrderSeparation: true,
 importOrderSortSpecifiers: true,
   overrides: [
@@ -42,4 +56,3 @@ importOrderSortSpecifiers: true,
     },
   ],
 };
-/** @exports prettier-config-solidity */
